9b99830a910ee6dae69703072ef3697f797bc08e
Modif93 <57270858+Modif93@users.noreply.github.com>
2024-08-26 10:41:39 +0900

    main script for standalone added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import uvicorn
-
-
+import os
 
 if __name__ == '__main__':
-    uvicorn.run("webapp.application:app",log_config="log.ini")
+    host = os.getenv("SERVER__HOST")
+    port = os.getenv("SERVER__PORT")
+    uvicorn.run("webapp.application:app",
+                host=host,
+                port=int(port), reload=True,
 
+                log_config="log.ini")
