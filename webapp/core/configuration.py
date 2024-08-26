e2366de17bb5346add1349c256296b2fee731d3d
Modif93 <57270858+Modif93@users.noreply.github.com>
2024-08-26 10:40:44 +0900

    setting example added

--- webapp/core/configuration.py
+++ webapp/core/configuration.py
@@ -2,3 +2,30 @@
 # from pydantic import BaseModel
 # class DatabaseConfig(Base):
 #
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DataSourceConfig(BaseModel):
+    dialect: str
+    driver: str
+    username: str
+    password: str
+    host: str
+    port: int
+    database: str
+
+
+class ServerConfig(BaseModel):
+    host: str
+    port: int
+
+
+class EnvConfig(BaseSettings):
+    datasource: DataSourceConfig
+    server: ServerConfig
+
+    model_config = SettingsConfigDict(env_nested_delimiter='__')
+
+
+env_config = EnvConfig()
--- webapp/core/database.py
+++ webapp/core/database.py
@@ -1,5 +1,5 @@
 from logging import getLogger
-
+from sqlalchemy import URL
 from sqlmodel import create_engine, SQLModel, Session
 
 logger = getLogger(__name__)
@@ -8,6 +8,20 @@ connect_url = "sqlite:///./webapp.db"
 
 engine = create_engine(connect_url, echo=True)
 
+
+def get_db_conn_url(dialect, driver, username, password, host, port, database):
+
+    if dialect == 'sqlite':
+        return f"sqlite:///{database}"
+
+    return URL.create(
+        drivername=f"{dialect}+{driver}",
+        username=username,
+        password=password,
+        host=host,
+        port=port,
+        database=database,
+    )
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
